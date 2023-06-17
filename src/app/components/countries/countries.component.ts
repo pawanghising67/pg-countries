1c4033b009b7003c05789f14291ac6d48fbeef2e
[Luna Shrestha] <[luna.shrestha@mnsu.edu]>
2023-06-16 20:01:12 -0500

    here you have a countires table but currrencies is not working for now. althoug a samll fix I am moving on to next project

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -9,6 +9,18 @@ import { CountriesService } from 'src/app/services/countries.service';
 export class CountriesComponent implements OnInit {
   
   public data:any;
+
+  getCurrencies(array:any){
+    array.forEach((element:any) => {
+      if(array.hasOwnProperty(element)){
+        const currency = array[element];
+        const currencyName = currency.name;
+        console.log(currencyName);
+        return currencyName;
+      }
+    });
+    
+  }
   //angular js will inject the service in runtime
   constructor(private _service:CountriesService) { }
   ngOnInit(): void {
